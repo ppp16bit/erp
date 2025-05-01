a3f1db6648c2044f7cfd45109ef640ff0b25bd25
ppp16bit <pcampossobrinho@gmail.com>
2025-05-01 19:01:58 -0300

    [feat] sale repositories

---
+++ src/main/java/ERP/business/management/repositories/SaleItemRepository.java
@@ -0,0 +1,11 @@
+package ERP.business.management.repositories;
+
+import ERP.business.management.model.sale.SaleItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SaleItemRepository extends JpaRepository {
+    List<SaleItem> findBySaleId(UUID sale_id);
+}
---
+++ src/main/java/ERP/business/management/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package ERP.business.management.repositories;
+
+import ERP.business.management.model.sale.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SaleRepository extends JpaRepository {
+    List<Sale> findByCustomerId(UUID customer_id);
+}
